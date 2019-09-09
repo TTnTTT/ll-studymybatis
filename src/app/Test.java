93d15055c5d6a8b5971d586de2a3c8016b2bc816
TTnTT <2e545>
2019-09-09 13:50:17 +0800

    第二周第一节课

--- src/app/Test.java
+++ src/app/Test.java
@@ -8,6 +8,8 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import domain.Student;
 public class Test {
 
 		public static void main(String[] args) {
@@ -23,14 +25,39 @@ public class Test {
 				//根据配置文件生成SqlSessionFactory对象
 				SqlSessionFactory sqlSessionFactory=new SqlSessionFactoryBuilder().build(inputStream);
 				//sqlSession 获取
-				sqlSession=sqlSessionFactory.openSession();
+				//sqlSession=sqlSessionFactory.openSession(true);//加了一个true为自动提交到数据库，默认不提交
+				sqlSession=sqlSessionFactory.openSession();//不加ture，进行事务管理
 				//执行查询请求
-				List<Map> list = sqlSession.selectList("findUserByStuName","马云");
+				//1List<Map> list = sqlSession.selectList("findUserByStuName","马云");
+				Student student=new Student();//调用数据类
+				
+				student.setStuName("马云");
+				student.setStuPassword("123456");
+				Student student1=sqlSession.selectOne("selectOne",student);
+				System.out.println(student1.getStuName()+" "+student1.getStuNo());
+				
+				//3List<Student> list = sqlSession.selectList("findUserByStudentinfo",student);
+				//2List<Map> list = sqlSession.selectList("findUserByStuNameAndPassword",student);
 				//输出查询结果
-				for(Map map : list){
+				/*2for(Map map : list){
 
 					System.out.println(map);
-				}
+				}*/
+				/*3for(Student student2 : list){
+					System.out.println(student2.getStuName());//只返回学生姓名
+				}*/
+				//插入
+				/*student.setStuName("马化腾");
+				student.setStuPassword("111111");
+				sqlSession.insert("insertStudent",student);*/
+				//修改密码
+				/*student.setStuName("马化腾");
+				student.setStuPassword("222222");
+				sqlSession.insert("updatePassword",student);*/
+				//删除用户
+				/*student.setStuName("雷军");
+				sqlSession.delete("deleteStudent",student);*/
+				
 				System.out.println("你好，测试");
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
--- src/domain/Student.java
+++ src/domain/Student.java
@@ -5,28 +5,37 @@ package domain;
  *
  */
 public class Student {
-private String no;//学号
-private String name;//学生姓名
+private String stuNo;//学号
+private String stuName;//学生姓名
+private String stuPassword;//学生密码
 
 public Student() {
 	super();
 	// TODO Auto-generated constructor stub
 }
-public Student(String no) {
-	this.no=no;
+public Student(String no,String stuPassword) {
+	this.stuNo=no;
+	this.stuPassword=stuPassword;
 	// TODO Auto-generated constructor stub
 }
-public String getNo() {
-	return no;
+public String getStuNo() {
+	return stuNo;
 }
-public void setNo(String no) {
-	this.no = no;
+public void setStuNo(String stuNo) {
+	this.stuNo = stuNo;
 }
-public String getName() {
-	return name;
+public String getStuName() {
+	return stuName;
 }
-public void setName(String name) {
-	this.name = name;
+public void setStuName(String stuName) {
+	this.stuName = stuName;
 }
+public String getStuPassword() {
+	return stuPassword;
+}
+public void setStuPassword(String stuPassword) {
+	this.stuPassword = stuPassword;
+}
+
 
 }
